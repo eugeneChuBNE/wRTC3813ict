76665180bcec52a2d59c6e2a6ffa0360039f6a75
Chu Hiển Đạt <chuhiendat@Chus-Laptop.local>
2023-10-21 17:52:35 +1000

    promote-user is fully implemented

--- new_frontend/newRTC/src/app/promote-user/promote-user.component.ts
+++ new_frontend/newRTC/src/app/promote-user/promote-user.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
-import { AuthService } from '../services/auth.service'; 
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-promote-user',
@@ -11,20 +11,31 @@ import { AuthService } from '../services/auth.service';
 export class PromoteUserComponent implements OnInit {
   form!: FormGroup;
   users: any[] = [];
+  groups: any[] = [];
+  selectedGroups: string[] = [];
+  showGroups: boolean = false;
+  selectedUser: any = null;
 
   constructor(
     private formBuilder: FormBuilder,
     private http: HttpClient,
-    private authService: AuthService // make sure the AuthService is injected
+    private authService: AuthService
   ) { }
 
   ngOnInit(): void {
     this.form = this.formBuilder.group({
-      userId: '',
-      role: ''
+      userId: ['', Validators.required],
+      role: ['', Validators.required]
     });
 
-    // Log the current user's username and role
+    this.form.get('userId')!.valueChanges.subscribe(userId => {
+      this.onUserChange(userId);
+    });
+
+    this.form.get('role')!.valueChanges.subscribe(role => {
+      this.onRoleChange(role);
+    });
+
     const currentUser = this.authService.currentUserValue;
     if (currentUser) {
       console.log(`Authenticated user - Username: ${currentUser.username}, Role: ${currentUser.role}`);
@@ -33,6 +44,7 @@ export class PromoteUserComponent implements OnInit {
     }
 
     this.fetchUsers();
+    this.fetchGroups();
   }
 
   fetchUsers(): void {
@@ -40,7 +52,6 @@ export class PromoteUserComponent implements OnInit {
       .subscribe(
         (res: any) => {
           this.users = res;
-          // Log each user's name and role from the fetched list
           this.users.forEach(user => {
             console.log(`Fetched User: ${user.username}, Role: ${user.role}`);
           });
@@ -51,10 +62,52 @@ export class PromoteUserComponent implements OnInit {
       );
   }
 
+  fetchGroups(): void {
+    this.http.get('http://localhost:3000/api/groups', { withCredentials: true })
+      .subscribe(
+        (res: any) => {
+          this.groups = res;
+        },
+        err => {
+          console.error('There was an error fetching the groups', err);
+        }
+      );
+  }
+
+  onUserChange(userId: string): void {
+    this.selectedUser = this.users.find(user => user._id === userId);
+    if (this.selectedUser) {
+      this.groups = this.groups.map(group => ({
+        ...group,
+        disabled: this.isUserModOfGroup(group._id),
+      }));
+    }
+  }
+
+  onRoleChange(role: string): void {
+    this.showGroups = role === 'mod';
+    if (!this.showGroups) {
+      this.selectedGroups = [];
+    }
+  }
+
+  onGroupSelect(event: any, groupId: string): void {
+    if (event.target.checked) {
+      this.selectedGroups.push(groupId);
+    } else {
+      this.selectedGroups = this.selectedGroups.filter(id => id !== groupId);
+    }
+  }
+
+  isUserModOfGroup(groupId: string): boolean {
+    return this.selectedUser?.groups?.includes(groupId) ?? false; // Safe navigation and defaulting to false if undefined
+  }
+
   submit(): void {
     this.http.patch(`http://localhost:3000/api/users/${this.form.value.userId}/role`, 
       {
-        role: this.form.value.role
+        role: this.form.value.role,
+        groupIds: this.selectedGroups
       }, 
       {
         withCredentials: true
@@ -62,8 +115,7 @@ export class PromoteUserComponent implements OnInit {
     ).subscribe(
       res => {
         console.log('User role updated', res);
-        // Refetch the user list to update the UI with the latest data
-        this.fetchUsers();
+        this.fetchUsers(); // Refresh the list of users
       },
       err => {
         console.error('There was an error updating the user role', err);
--- new_server/routes/adminRoutes.js
+++ new_server/routes/adminRoutes.js
@@ -9,40 +9,46 @@ const { requireAdmin } = require('../middleware/auth');
 // Promote a user to admin or mod (Only an admin can do this)
 router.patch('/users/:id/role', requireAdmin, async (req, res) => {
     try {
-        const { role, groupId } = req.body; // groupId is the ID of the group to which the user will be assigned
-
-        if (!groupId && role === 'mod') {
-            return res.status(400).send({ message: 'groupId is required to promote a user to mod' });
-        }
+        const { role, groupIds } = req.body; // groupIds is an array of IDs of the groups to which the user will be assigned as a mod
 
         const user = await User.findById(req.params.id);
         if (!user) {
             return res.status(404).send({ message: 'User not found' });
         }
 
-        // If the user is being promoted to 'mod', we need to handle group assignment
+        // Promoting a user to 'mod'
         if (role === 'mod') {
-            const group = await Group.findById(groupId);
-            if (!group) {
-                return res.status(404).send({ message: 'Group not found' });
-            }
+            // If groupIds are provided, we'll add the user as a mod and a member to these groups
+            if (groupIds && Array.isArray(groupIds) && groupIds.length > 0) {
+                await Promise.all(groupIds.map(async (groupId) => {
+                    const group = await Group.findById(groupId);
+                    if (!group) {
+                        return Promise.reject({ message: `Group not found with ID: ${groupId}` });
+                    }
 
-            // Check if the user is already a member of the group; if not, add them
-            if (!group.members.includes(user._id)) {
-                group.members.push(user._id);
-            }
+                    // Check if the user is already a member of the group; if not, add them
+                    if (!group.members.includes(user._id)) {
+                        group.members.push(user._id);
+                    }
 
-            // If the user is not already a mod, add them to the mods list
-            if (!group.mods.includes(user._id)) {
-                group.mods.push(user._id);
-            }
+                    // If the user is not already a mod, add them to the mods list
+                    if (!group.mods.includes(user._id)) {
+                        group.mods.push(user._id);
+                    }
 
-            // Save the updated group information
-            await group.save();
+                    // Save the updated group information
+                    await group.save();
+
+                    // Ensure the user is part of these groups in the user's document as well
+                    if (!user.groups.includes(groupId)) {
+                        user.groups.push(groupId);
+                    }
+                }));
+            }
         }
 
         // Update the user's role
-        user.role = role;
+        user.role = [role];
         await user.save();
 
         res.send({ message: 'Role updated successfully' });
@@ -51,6 +57,7 @@ router.patch('/users/:id/role', requireAdmin, async (req, res) => {
         res.status(500).send({ message: error.message });
     }
 });
+
 
 // Route to fetch all users (accessible only by admins)
 router.get('/users', requireAdmin, async (req, res) => {
